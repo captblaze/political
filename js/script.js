3d636a00e789ec902cbaa82af93e81e0e714503e
captblaze <standbymii@yahoo.com>
2021-11-24 11:21:27 +0100

    POLITICAL

--- js/script.js
+++ js/script.js
@@ -2,32 +2,29 @@ $(document).ready(function() {
     $("#insurance").submit(function(event){
 
     const religion =$("select#religion").val();
-    const aims =$("select#aims").val();
     const passion =$("select#passion").val();
   
-    if ( religion=== "xtian" || "muslim" && aims === "education"||"infrastructure"||"roads"||"health"||"employment" && passion === "reading" ) {
-      console.log('it works');
+    if ( passion === "reading" ) {
       $('#maturedFem').show();
-    } else if ( religion=== "xtian" || "muslim" && aims === "education"||"infrastructure"||"roads"||"health"||"employment" && passion === "travelling" ) {
 
-      console.log('it works');
+    } else if (passion === "travelling" ) {
 
-      
-      $('#youngFem').show();
-    } else if ( religion=== "xtian" || "muslim" && aims === "education"||"infrastructure"||"roads"||"health"||"employment" && passion === "womanizing" ||"drinking" ) {
+      $('.youngFem').show();
+    
+    } else if (passion === "womanizing") {
+     
+        $('.maturedMale').show(); 
+    
+      } else if ( passion === "drinking") {
+     
+        $('.maturedMale').show(); 
 
-      console.log('it works');
+    } else if ( passion === "building"){
 
-      
-        $('#maturedMale').show(); 
-    } else if ( religion=== "xtian"|| "muslim" && aims === "education"||"infrastructure"||"roads"||"health"||"employment" && passion === "building" ){
-      console.log('it works');
-     $('#youngMale').show();  
+      $('.youngMale').show();  
     }else {
 
-      console.log('it works');
-
-     $('Please fill the form correctly or leave my dating app').show();
+     $('Please fill the form correctly or leave my app').show();
     }
     event.preventDefault();
 
